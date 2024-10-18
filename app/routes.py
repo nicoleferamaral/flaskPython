b8bddf9e63767d338767f444e9a34ba7b5ca638e
Nicole Ferreira <nicoleferamaral@gmail.com>
2024-10-18 16:45:30 -0300

    Final Commit

--- app/routes.py
+++ app/routes.py
@@ -26,13 +26,14 @@ def cadastrarUsuario():
     try: #Vai tentar executar
         cpf = request.form.get("cpf")  #coletar dados
         nome = request.form.get("nome")
-        telefone = request.form.get("telefone")
-        endereco = request.form.get("endereco")
         email = request.form.get("email")
         senha = request.form.get("senha")
+        telefone = request.form.get("telefone")
+        endereco = request.form.get("endereco")
 
 
-        dados = {"cpf": cpf, "nome":nome, "telefone":telefone, "endereco": endereco, "email":email, "senha":senha} #formato de requisição, coleção de dados, qual é o dado e quem é o dado
+
+        dados = {"cpf": cpf, "nome":nome, "email":email, "senha":senha, "telefone":telefone, "endereco": endereco} #formato de requisição, coleção de dados, qual é o dado e quem é o dado
         requisicao = requests.post(f'{link}/cadastro/.json', data = json.dumps(dados)) #resposta true or false, vai tentar inserir no banco de dados os dados informados
         return 'Cadastrado com sucesso!'
     except Exception as e: #caso tenha uma excessão roda o exception
@@ -61,48 +62,74 @@ def listarIndividual():
             chave = dicionario[codigo]['nome']
             if chave == procurar:
 
-                return f'Nome: {dicionario[codigo]["nome"]}\n<br> CPF: {dicionario[codigo]["cpf"]}\n<br> Telefone: {dicionario[codigo]["telefone"]}\n<br> Endereco: {dicionario[codigo]["endereco"]}\n<br>'
+                return f'Nome: {dicionario[codigo]["nome"]}\n<br> CPF: {dicionario[codigo]["cpf"]}\n<br> Email: {dicionario[codigo]["email"]}\n<br> Senha: {dicionario[codigo]["senha"]}\n<br> Telefone: {dicionario[codigo]["telefone"]}\n<br> Endereco: {dicionario[codigo]["endereco"]}\n<br>'
 
     except Exception as e:
         return f'Algo deu errado\n {e}'
 
+
 @app.route('/atualizacao')
 def atualizacao():
-    return render_template('atualizacao.html',titulo="Atualização")
+    return render_template('atualizacao.html', titulo="Atualizacao")
+
 
 @app.route('/atualizar', methods=['POST'])
 def atualizar():
     try:
-        requesicao = requests.get(f'{link}/consultar/.json')
+        requesicao = requests.get(f'{link}/cadastro/.json')
         dicionario = requesicao.json()
 
-        cpf      = request.form.get("cpf")
-        nome     = request.form.get("nome")
+        cpf = request.form.get("cpf")
+        nome = request.form.get("nome")
+        email = request.form.get("email")
+        senha = request.form.get("senha")
         telefone = request.form.get("telefone")
         endereco = request.form.get("endereco")
-        dados    = {"cpf": cpf, "nome": nome, "telefone": telefone, "endereco": endereco}
+        dados = {"cpf": cpf, "nome": nome, "email": email, "senha": senha, "telefone": telefone, "endereco": endereco}
 
         for codigo in dicionario:
-            chave = dicionario[codigo][cpf]
+            chave = dicionario[codigo]['cpf']
             if chave == cpf:
-                requisicao = requests.patch(f'{link}/consultar/{codigo}/.json', data=json.dumps(dados))
+                requisicao = requests.patch(f'{link}/cadastro/{codigo}/.json', data=json.dumps(dados))
                 return "Atualizado com sucesso!"
 
     except Exception as e:
         return f'Algo deu errado\n {e}'
 
 
-@app.route('/excluir')
-def excluir():
+@app.route('/deleta', methods=['POST'])
+def deleta():
     try:
-        requisicao = requests.delete(f'{link}/cadastro/-O8mkk11M1SdppmuhJcc/.json')
-        return f'Excluido com sucesso'
+        requisicao = requests.get(f'{link}/cadastro/.json')
+        dicionario = requisicao.json()
+
+        cpf = request.form.get("cpf")
+        nome = request.form.get("nome")
+
+        for codigo in dicionario:
+            chave = dicionario[codigo]['cpf']
+            if chave == cpf:
+                requisicao = requests.delete(f'{link}/cadastro/{codigo}/.json')
+                return "Excluido com sucesso!"
+
 
     except Exception as e:
-        return f'Algo deu errado\n {e}'
+        return f'Algo deu errado\n +{e}'
+
 
 @app.route('/consultar')
 def consultar():
     return render_template('consultar.html', titulo="Consultar")
 
 
+@app.route('/excluir')
+def excluir():
+    return render_template('excluir.html', titulo="Excluir")
+
+
+@app.route('/resposta', methods=['POST'])
+def resposta():
+    try:
+        return "Mensagem enviada com sucesso!"
+    except Exception as e:
+        return f'Algo deu errado\n +{e}'
